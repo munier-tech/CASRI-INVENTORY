f1b4acf64dc7bce995e19ad8c0820c4862fbc3c6
munier-tech <muniry399@gmail.com>
2025-09-04 09:13:37 +0300

    done

--- FRONTEND1/src/lib/axios.js
+++ FRONTEND1/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL: import.meta.env.VITE_REACT_APP_API_URL || (import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api"),
   withCredentials: true,
 });
 
